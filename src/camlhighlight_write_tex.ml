43f7dbd37365e61bf638f2e79b1dbaaafee6a14d
darioteixeira <darioteixeira@yahoo.com>
2009-07-22 11:44:08 +0000

    Added skeleton implementation for TeX writer

---
+++ src/camlhighlight_write_tex.ml
@@ -0,0 +1,15 @@
+(********************************************************************************)
+(*	Camlhighlight_write_text.ml
+	Copyright (c) 2009 Dario Teixeira (dario.teixeira@yahoo.com)
+	This software is distributed under the terms of the GNU GPL version 2.
+	See LICENSE file for full license text.
+*)
+(********************************************************************************)
+
+
+(********************************************************************************)
+(**	{2 Public functions}							*)
+(********************************************************************************)
+
+let write hilite = "not yet implemented"
+
