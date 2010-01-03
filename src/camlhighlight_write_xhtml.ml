e7e5b274850d06cf60b9b19ecce81adfbc0d97df
darioteixeira <darioteixeira@yahoo.com>
2010-01-03 14:21:24 +0000

    Now using <pre> instead of <p> to wrap source code

--- src/camlhighlight_write_xhtml.ml
+++ src/camlhighlight_write_xhtml.ml
@@ -26,8 +26,8 @@ open Camlhighlight_core
 *)
 let write ?(class_prefix = "hl_") ?(extra_classes = []) ?(dummy_lines = true) ?(linenums = false) code =
 	let make_class ?(extra_classes = []) names = a_class (extra_classes @ (List.map (fun x -> class_prefix ^ x) names)) in
-	let normal_line content = XHTML.M.span ~a:[make_class ["line"]] content in
-	let dummy = if dummy_lines then [XHTML.M.span ~a:[make_class ["line"; "dummy"]] []] else [] in
+	let normal_line content = [pcdata "\n"; XHTML.M.code ~a:[make_class ["line"]] content] in
+	let dummy = if dummy_lines then [pcdata "\n"; XHTML.M.code ~a:[make_class ["line"; "dummy"]] []] else [] in
 	let elem_to_xhtml = function
 		| Default s	       -> XHTML.M.pcdata s
 		| Special (special, s) -> XHTML.M.span ~a:[make_class [special]] [XHTML.M.pcdata s] in
@@ -35,10 +35,10 @@ let write ?(class_prefix = "hl_") ?(extra_classes = []) ?(dummy_lines = true) ?(
 		let code_len = List.length code in
 		let width = String.length (string_of_int code_len) in
 		let numline_to_xhtml num = normal_line [XHTML.M.pcdata (Printf.sprintf "%0*d" width num)]
-		in XHTML.M.p ~a:[make_class ["nums"]] (dummy @ (List.map numline_to_xhtml (List.init code_len (fun x -> x+1))) @ dummy)
+		in XHTML.M.pre ~a:[make_class ["nums"]] (dummy @ (List.flatten (List.map numline_to_xhtml (List.init code_len (fun x -> x+1)))) @ dummy)
 	and convert_code () =
 		let codeline_to_xhtml line = normal_line (List.map elem_to_xhtml line)
-		in XHTML.M.p ~a:[make_class ["code"]] (dummy @ (List.map codeline_to_xhtml code) @ dummy)
+		in XHTML.M.pre ~a:[make_class ["code"]] (dummy @ (List.flatten (List.map codeline_to_xhtml code)) @ dummy)
 	in XHTML.M.div ~a:[make_class ~extra_classes ["main"]]
 		(match linenums with
 			| true	-> [convert_nums (); convert_code ()]
