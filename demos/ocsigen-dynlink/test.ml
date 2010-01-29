51193bd50da58494809a6b105995a2144fa053e8
darioteixeira <darioteixeira@yahoo.com>
2010-01-29 18:38:04 +0000

    Fixed docs

--- demos/ocsigen-dynlink/test.ml
+++ demos/ocsigen-dynlink/test.ml
@@ -1,5 +1,5 @@
 (********************************************************************************)
-(*	main.ml
+(*	test.ml
 	Copyright (c) 2010 Dario Teixeira (dario.teixeira@yahoo.com)
 	This software is distributed under the terms of the GNU GPL version 2.
 	See LICENSE file for full license text.
