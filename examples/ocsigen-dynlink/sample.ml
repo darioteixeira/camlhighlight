79e7bd8cd4e3393485f97eb8021340db64dbe4f1
darioteixeira <darioteixeira@yahoo.com>
2013-07-21 11:34:45 +0000

    Removed 'doc' dir

---
+++ examples/ocsigen-dynlink/sample.ml
@@ -0,0 +1,5 @@
+type foo_t =
+	| One
+	| Two
+	| Three
+
--- examples/ocsigen-dynlink/test.ml
+++ examples/ocsigen-dynlink/test.ml
@@ -21,17 +21,32 @@ let test_service =
 
 
 let test_handler () () =
-	let ch = open_in "test.ml" in
-	let str = Std.input_all ch in
+	let ch = open_in "sample.ml" in
+	let str = BatPervasives.input_all ch in
 	let () = close_in ch in
 	let () = Camlhighlight_parser.set_tabspaces 8 in
 	let hilite = Camlhighlight_parser.from_string ~lang:"caml" str in
+	let sexp = Camlhighlight_core.sexp_of_t hilite in
+	let sexp_str = Sexplib.Sexp.to_string_hum sexp in
 	let hilite_xhtml = Camlhighlight_write_html5.write ~linenums:true ~extra_classes:["hl_zebra"] hilite in
+	let hilite_str =
+		let buf = Buffer.create 100 in
+		Html5.Printer.print_list ~output:(Buffer.add_string buf) [hilite_xhtml];
+		Buffer.contents buf in
 	let css_uri = make_uri (Eliom_service.static_dir ()) ["css"; "highlight.css"]
 	in Lwt.return
 		(html
 			(head (title (pcdata "Test")) [css_link ~a:[(a_media [`All]); (a_title "Default")] ~uri:css_uri ()])
-			(body [hilite_xhtml]))
+			(body	[
+				h1 [pcdata "Original source:"];
+				pre [pcdata str];
+				h1 [pcdata "S-expression:"];
+				pre [pcdata sexp_str];
+				h1 [pcdata "Raw HTML5:"];
+				pre [pcdata hilite_str];
+				h1 [pcdata "Rendered HTML5:"];
+				hilite_xhtml;
+				]))
 
 
 let () = Eliom_registration.Html5.register test_service test_handler
