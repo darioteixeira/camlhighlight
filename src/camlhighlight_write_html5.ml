b72f737d3c4c8d198435de0fab978cf80ea435db
darioteixeira <darioteixeira@yahoo.com>
2015-12-26 13:57:00 +0000

    Split Camlhighlight_write_html5.write into write_inline/write_block variants

--- src/camlhighlight_write_html5.ml
+++ src/camlhighlight_write_html5.ml
@@ -19,26 +19,37 @@ module Make (Html5: Html5_sigs.T with type 'a Xml.wrap = 'a and type 'a wrap = '
 struct
     open Html5
 
-    let write ?(class_prefix = "hl_") ?(extra_classes = []) ?(dummy_lines = true) ?(linenums = false) source =
-        let make_class ?(extra_classes = []) names = a_class (extra_classes @ (List.map (fun x -> class_prefix ^ x) names)) in
-        let normal_line content = [pcdata "\n"; code ~a:[make_class ["line"]] content] in
-        let dummy = if dummy_lines then [pcdata "\n"; code ~a:[make_class ["line"; "dummy"]] []] else [] in
-        let class_of_special special =
-            Sexplib.Sexp.to_string_mach (Camlhighlight_core.sexp_of_special_style special) in
-        let elem_to_xhtml = function
-            | (#normal_style, str)             -> pcdata str
-            | (#special_style as special, str) -> span ~a:[make_class [class_of_special special]] [pcdata str] in
+    let make_class ~class_prefix ?(extra_classes = []) names =
+        a_class (List.fold_right (fun x accum -> (class_prefix ^ x) :: accum) names extra_classes)
+
+    let class_of_special special =
+        Sexplib.Sexp.to_string_mach (Camlhighlight_core.sexp_of_special_style special)
+
+    let elem_to_xhtml ~class_prefix = function
+        | (#normal_style, str)             -> pcdata str
+        | (#special_style as special, str) -> span ~a:[make_class ~class_prefix [class_of_special special]] [pcdata str]
+
+    let write_inline ?(class_prefix = "hl_") ?extra_classes = function
+        | [line] -> span ~a:[make_class ~class_prefix ?extra_classes ["inline"]] (List.map (elem_to_xhtml ~class_prefix) line)
+        | _      -> invalid_arg "write_inline"
+
+    let write_block ?(class_prefix = "hl_") ?extra_classes ?(dummy_lines = true) ?(linenums = false) source =
+        let dummy = if dummy_lines then [pcdata "\n"; code ~a:[make_class ~class_prefix ["line"; "dummy"]] []] else [] in
+        let normal_line content =
+            [pcdata "\n"; code ~a:[make_class ~class_prefix ["line"]] content] in
         let convert_nums () =
             let source_len = List.length source in
             let width = String.length (string_of_int source_len) in
             let numline_to_xhtml num = normal_line [pcdata (Printf.sprintf "%0*d" width num)]
-            in pre ~a:[make_class ["nums"]] (dummy @ (List.flatten (List.map numline_to_xhtml (List.init source_len (fun x -> x+1)))) @ dummy)
+            in pre ~a:[make_class ~class_prefix ["nums"]] (dummy @ (List.flatten (List.map numline_to_xhtml (List.init source_len (fun x -> x+1)))) @ dummy)
         and convert_source () =
-            let source_line_to_xhtml line = normal_line (List.map elem_to_xhtml line)
-            in pre ~a:[make_class ["code"]] (dummy @ (List.flatten (List.map source_line_to_xhtml source)) @ dummy)
-        in div ~a:[make_class ~extra_classes ["main"]]
+            let source_line_to_xhtml line = normal_line (List.map (elem_to_xhtml ~class_prefix) line)
+            in pre ~a:[make_class ~class_prefix ["code"]] (dummy @ (List.flatten (List.map source_line_to_xhtml source)) @ dummy)
+        in div ~a:[make_class ~class_prefix ?extra_classes ["block"]]
             (match linenums with
                 | true  -> [convert_nums (); convert_source ()]
                 | false -> [convert_source ()])
+
+        let write = write_block
 end
 
--- src/camlhighlight_write_html5.mli
+++ src/camlhighlight_write_html5.mli
@@ -30,14 +30,38 @@
 module Make: functor (Html5: Html5_sigs.T with type 'a Xml.wrap = 'a and type 'a wrap = 'a and type 'a list_wrap = 'a list) ->
 sig
     (** This function converts a value of type {!Camlhighlight_core.t} containing
-        a syntax-highlighted document into its [Html5] representation.
-        The optional parameter [class_prefix] indicates the prefix for the class
-        names of all HTML5 elements produced, while [extra_classes] can be used
-        to provide additional class names for the main container.  Also optional
-        are the boolean parameters [dummy_lines], and [linenums].  They indicate
-        whether the generated HTML5 should include dummy lines at the beginning
-        and end, and line numbers for the code, respectively.
+        a syntax-highlighted document into a [Html5] [span] element.  Note that
+        the document must consist of a single line; otherwise an [Invalid_argument]
+        exception is raised.  The optional parameter [class_prefix] indicates the
+        prefix for the class names of all HTML5 elements produced, while [extra_classes]
+        can be used to provide additional class names for the main container (these
+        extra classes are not prefixed by [class_prefix]).
     *)
+    val write_inline:
+        ?class_prefix:string ->
+        ?extra_classes:Html5_types.nmtoken list ->
+        Camlhighlight_core.t ->
+        [> Html5_types.span ] Html5.elt
+
+    (** This function converts a value of type {!Camlhighlight_core.t} containing
+        a syntax-highlighted document into a [Html5] [div] element.  The optional
+        parameter [class_prefix] indicates the prefix for the class names of all
+        HTML5 elements produced, while [extra_classes] can be used to provide
+        additional class names for the main container (these extra classes are not
+        prefixed by [class_prefix]).  Also optional are the boolean parameters
+        [dummy_lines], and [linenums].  They indicate whether the generated HTML5
+        should include dummy lines at the beginning and end, and line numbers for
+        the code, respectively.
+    *)
+    val write_block:
+        ?class_prefix:string ->
+        ?extra_classes:Html5_types.nmtoken list ->
+        ?dummy_lines:bool ->
+        ?linenums:bool ->
+        Camlhighlight_core.t ->
+        [> Html5_types.div ] Html5.elt
+
+    (** @deprecated [write] is an alias for {!write_block}. *)
     val write:
         ?class_prefix:string ->
         ?extra_classes:Html5_types.nmtoken list ->
@@ -45,5 +69,6 @@ sig
         ?linenums:bool ->
         Camlhighlight_core.t ->
         [> Html5_types.div ] Html5.elt
+        [@@ocaml.deprecated "Use write_block instead."]
 end
 
