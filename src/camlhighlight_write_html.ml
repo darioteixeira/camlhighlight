4f57eb43ae8f7e0004e50e2b9c3c72bb88e9da46
darioteixeira <darioteixeira@yahoo.com>
2016-05-23 16:16:15 +0100

    Update copyright dates and email

--- src/camlhighlight_write_html.ml
+++ src/camlhighlight_write_html.ml
@@ -1,6 +1,6 @@
 (********************************************************************************)
 (*  Camlhighlight_write_html.ml
-    Copyright (c) 2010-2014 Dario Teixeira (dario.teixeira@yahoo.com)
+    Copyright (c) 2010-2016 Dario Teixeira <dario.teixeira@nleyten.com>
     This software is distributed under the terms of the GNU GPL version 2.
     See LICENSE file for full license text.
 *)
